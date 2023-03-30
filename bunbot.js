b31d6677ffaca296a705214e440f19f8aafdae47
georgie <64393689+geoshad@users.noreply.github.com>
2023-03-30 23:00:33 +1100

    minor updates

--- bunbot.js
+++ bunbot.js
@@ -6,14 +6,22 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ]});
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // bot login
 client.login(process.env.CLIENT_TOKEN);
 
+// replies with a greeting
+client.on("messageCreate", (message) => {
+    if (message.content === "hi!") {
+        message.reply("hi!");
+    }
+});
+
+// replies with "pong" when you ping the bot
 client.on("messageCreate", (message) => {
     if (message.mentions.has(client.user)) {
-        message.reply("Pong!");
+        message.reply("pong!");
     }
 });
